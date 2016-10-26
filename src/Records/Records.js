64222489315c1f84d005f776e279ac5529aedb0f
Misha Moroshko <michael.moroshko@gmail.com>
2016-10-27 00:00:21 +1100

    Format record date

--- src/Records/Records.js
+++ src/Records/Records.js
@@ -2,6 +2,20 @@ import React, { Component, PropTypes } from 'react';
 import './Records.css';
 
 const MAX_RECORDS = 100;
+const MONTHS = {
+  '01': 'Jan',
+  '02': 'Feb',
+  '03': 'Mar',
+  '04': 'Apr',
+  '05': 'May',
+  '06': 'Jun',
+  '07': 'Jul',
+  '08': 'Aug',
+  '09': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec'
+};
 
 class Records extends Component {
   static contextTypes = {
@@ -46,13 +60,32 @@ class Records extends Component {
     base.removeBinding(this.ref);
   }
 
-  renderRecord(record) {
+  renderDate(date) {
+    const currentYear = (new Date()).getFullYear();
+    const month = MONTHS[date.slice(5, 7)];
+    const day = date.slice(8, 10);
+
+    if (date.startsWith(currentYear)) {
+      return `${month} ${day}`;
+    }
+
+    const year = date.slice(0, 4);
+
+    return `${month} ${day}, ${year}`;
+  }
+
+  renderRecord = record => {
     return (
       <li key={record.key}>
-        {record.date}
+        <div>
+          {this.renderDate(record.date)}
+        </div>
+        <div>
+          {record.description}
+        </div>
       </li>
     );
-  }
+  };
 
   render() {
     const { loading, records } = this.state;
