b8d8a3d16a26de1db9e006faa0aa75c82c02ef65
Misha Moroshko <michael.moroshko@gmail.com>
2016-11-01 17:21:22 +1100

    Fix one digit day issue

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ export const getToday = () => {
   const month = now.getMonth() + 1;
   const day = now.getDate();
 
-  return `${year}-${month < 10 ? `0${month}` : month}-${day}`;
+  return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
 };
 
 const amountRegex = /^\d+(\.\d{1,2})?$/;
